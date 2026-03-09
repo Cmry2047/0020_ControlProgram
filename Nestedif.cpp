d063a945fcfe96e6c3083e07af7721aaf573fd4a
Cmry2047 <ahmadfadhilardiansyah7@gmail.com>
2026-03-09 11:04:09 +0700

    Menambahkan basic input nilai siswa denagn C++

---
+++ Nestedif.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float nilBI, nilMT;
+
+    cout << "Masukkan nilai Bahasa Inggris = ";
+    cin >> nilBI;
+
+    cout << "Masukkan nilai Matematika = ";
+    cin >> nilMT;
+}
