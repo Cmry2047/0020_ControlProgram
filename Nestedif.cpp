3f7d2e1617ce191b9404fcecc6af06406ff02186
Cmry2047 <ahmadfadhilardiansyah7@gmail.com>
2026-03-09 11:04:47 +0700

    Menambhakan penghitung rata rata nilai

--- Nestedif.cpp
+++ Nestedif.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 using namespace std;
 
+float Rata_rata(float a, float b)
+{
+    return (a + b) / 2;
+}
+
 int main()
 {
-    float nilBI, nilMT;
+    float nilBI, nilMT, rerata;
 
     cout << "Masukkan nilai Bahasa Inggris = ";
     cin >> nilBI;
 
     cout << "Masukkan nilai Matematika = ";
     cin >> nilMT;
+
+    rerata = Rata_rata(nilBI, nilMT);
+
+    cout << "Nilai rerata = " << rerata << endl;
 }
