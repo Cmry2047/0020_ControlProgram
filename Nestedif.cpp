a5f2dd80643cca3801d98c00c5856356b081930d
Cmry2047 <ahmadfadhilardiansyah7@gmail.com>
2026-03-09 11:07:14 +0700

    Hasil program kelulusan berdasar nilai lebih dari sama dengan 60

--- Nestedif.cpp
+++ Nestedif.cpp
@@ -17,14 +17,22 @@ string Status_lulus(float r)
 int main()
 {
     float nilBI, nilMT, rerata;
+    string status;
 
     cout << "Masukkan nilai Bahasa Inggris = ";
     cin >> nilBI;
-
     cout << "Masukkan nilai Matematika = ";
     cin >> nilMT;
 
-    rerata = Rata_rata(nilBI, nilMT);
+    // rerata = (nilBI+nilMT)/2;
+    // if (rerata >= 60)
+    //     status = "Lolos";
+    // else
+    //     status = "Gagal";
 
-    cout << "Status kelulusan = " << Status_lulus(rerata) << endl;
+    rerata = Rata_rata(nilBI, nilMT);
+    // status = Status_lulus(rerata);
+
+    cout << "Status kelulusan = " << Status_lulus(rerata)
+         << ",dengan nilai rerata = " << rerata << endl;
 }
